707ae984dd168c4931863304bdbdeb3b05373e64
Jean Pereira <jean.pereira@medsenior.com.br>
2022-10-25 15:36:50 -0300

    feature: Project start

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  trailingComma: 'all',
+  semi: true,
+  singleQuote: true,
+  printWidth: 120,
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  endOfLine: 'auto',
+  tabWidth: 2,
+};
